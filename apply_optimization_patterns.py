43fc929e4f3f76d5d2465a8858e5539daab54d52
Brion <briontechengineer@gmail.com>
2024-09-19 08:06:06 -0400

    Create apply_optimization_patterns.py

---
+++ apply_optimization_patterns.py
@@ -0,0 +1,4 @@
+infinite_mind_quantized = InfiniteMindQuantized()
+example_code = "example_function"
+optimized_code = infinite_mind_quantized.apply_optimization_patterns(example_code)
+print(optimized_code)
